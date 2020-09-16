ee8b320e0d3f1a0d1fe441d728b775c529283b52
devcaA25sa <faraha7@csp.edu>
2020-09-16 09:34:08 -0500

    fixed server.js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-/K/KN  K<
+const express = require('express');
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static(__dirname + '/dist/'));
+app.get(/.*/, function (req, res) {
+  res.sendFile(__dirname + '/dist/index.html');
+})
+app.listen(port);
+
+console.log("server started");
+
+// create a NEW github repo for this project
+// create a NEW heroku for this project   
+// push heroku
